89e128601e54d07828620d54f0f90d3e04f57373
harisvu9 <harisvu9@gmail.com>
2021-10-20 22:29:26 -0700

    hrb code for admin ec2 has been added-typo-6

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
     "qnt" = "hrb-qnt"
     "prd" = "hrb-prd"
   }
-  ckan_domain   = local.map_ckan_domain[var.stack]
-}
 
-admin_hst_role_name    = data.aws_iam_role.admin_role[0].id
+  ckan_domain   = local.map_ckan_domain[var.stack]
+  admin_hst_role_name    = var.vpc == "pw2prd" || var.vpc == "pe1prd" ? data.aws_iam_role.admin_role[0].id : join("", aws_iam_role.*.name)
+}
