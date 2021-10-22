952ab2e7d3df61734b8070bbecc1fc21504afd74
harisvu9 <harisvu9@gmail.com>
2021-10-21 19:56:52 -0700

    hrb code for admin ec2 has been added-typo-20

--- security_groups.tf
+++ security_groups.tf
@@ -127,14 +127,13 @@ resource "aws_security_group_rule" "loadbalancer_8443" {
 }
 
 resource "aws_security_group_rule" "loadbalancer_egress" {
-  security_group_id    = aws_security_group.loadbalancer.id
   description          = "Allow outbound to hrb network"
   type                 = "egress"
   from_port            = 0
   to_port              = 0
   protocol             = "-1"
   cidr_blocks          = ["10.0.0.0/8"]
-  security_group_id         = aws_security_group.loadbalancer.id
+  security_group_id    = aws_security_group.loadbalancer.id
 }
 
 # --------------------------------------
