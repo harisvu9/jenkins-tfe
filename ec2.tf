88951642d5930557b06a7068c0077d38588a8784
harisvu9 <harisvu9@gmail.com>
2021-11-25 12:00:28 -0800

    two Security Groups have been Formatted for EC2 with ids test

--- ec2.tf
+++ ec2.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "admin_ec2" {
   key_name               = var.key_name
   associate_public_ip_address = var.associate_public_ip_address
   # vpc_security_group_ids = [aws_security_group.admin.id, aws_security_group.domain.id]
-  security_groups        = concat([aws_security_group.admin.name, aws_security_group.domain.name])
+  security_groups        = [aws_security_group.admin.id, aws_security_group.domain.id]
   # security_groups        = ["hst-admin"]
   subnet_id              = "${element(tolist(data.aws_subnet_ids.all.ids), 1)}"
   tags                   = "${merge(var.tags, tomap({"Name" = var.instance_name}))}"
